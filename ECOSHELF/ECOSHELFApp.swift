7f0afb31088d14d9a99a3e997b491be11822d6a6
hazher <baxigshti@hotmail.de>
2026-03-14 14:19:59 +0100

    Initial Commit

---
+++ ECOSHELF/ECOSHELFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECOSHELFApp.swift
+//  ECOSHELF
+//
+//  Created by Hama on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ECOSHELFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
